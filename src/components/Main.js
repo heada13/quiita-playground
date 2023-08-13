416454bcd7f1d341dca7e907a053816e2e9bbc3a
heada13 <babolatlist_1003@yahoo.co.jp>
2023-08-13 15:04:39 +0900

    wip:usetransition実装

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,53 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Router, Routes, createBrowserRouter } from 'react-router-dom';
 // import Top from './Top';
 // import Second from './Second';
-import { lazy, Suspense } from 'react';
-
+import { lazy, Suspense, useState, useTransition } from 'react';
 const LazyTop = lazy(() => import('./Top'))
 const LazySecond = lazy(() => import('./Second'))
+
+
+// const SuspenseRouter = ({ children, history, ...config }) => {
+//   const [startTransition, isPending] = useTransition(config,() => ({
+//     from: { opacity: 0 },
+//     enter: { opacity: 1 },
+//     leave: { opacity: 1 },
+//   }));
+//   const [suspenseHistory] = useState(() => {
+//     const { push, replace, go } = history;
+
+//     history.push = (...args) => {
+//       startTransition(() => { push.apply(history, args); });
+//     };
+//     history.replace = (...args) => {
+//       startTransition(() => { replace.apply(history, args); });
+//     };
+//     history.go = (...args) => {
+//       startTransition(() => { go.apply(history, args); });
+//     };
+//   });
+
+//   suspenseHistory.isPending = isPending;
+
+//   return (
+//     <Router history={suspenseHistory}>
+//       {children}
+//     </Router>
+//   );
+// };
+
+// const history = createBrowserRouter();
 
 const Main = () => {
   return(
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<LazyTop/>}/>
-          <Route path='/mail' element={<LazySecond/>}/>
-        </Routes>
-      </Suspense>
+      {/* <SuspenseRouter history={history} timeoutMs={2000}> */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path='/' element={<LazyTop/>}/>
+            <Route path='/mail' element={<LazySecond/>}/>
+          </Routes>
+        </Suspense>
+      {/* </SuspenseRouter> */}
     </>
   )
 }
